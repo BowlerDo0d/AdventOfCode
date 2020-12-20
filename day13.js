5414862a04b82ab61b3fd2747a4c01b125c27bf8
Steve Mahony <mahonysr@upmc.edu>
2020-12-20 12:37:26 -0500

    day 13 shrug

--- day13.js
+++ day13.js
@@ -25,59 +25,87 @@ const day = 13,
 
 // console.log('Bus to take:', schedule[0].bus * schedule[0].waitTime);
 
-const routes = [],
-  offsets = [];
+// Part two (An attempt at CRT)
+// const routes = [],
+//   offsets = [];
 
+// input[1].split(',').forEach((bus, idx) => {
+//   if (bus !== 'x') {
+//     routes.push(+bus);
+//     offsets.push(idx);
+//   }
+// });
+
+// let prod = 1;
+// routes.forEach((route) => prod *= route);
+
+// function getXi(Ni, mod) {
+//   let a = Ni - (mod * Math.floor(Ni/mod));
+
+//   if (a === 1) {
+//     return a;
+//   }
+
+//   let i = 2;
+//   while ((i*a) % mod != 1) {
+//     i += 1;
+//   }
+
+//   return i;
+// }
+
+// function createBus(id, offset, N) {
+//   const bi = offset,
+//     mod = id,
+//     Ni = Math.floor(N / mod),
+//     xi = getXi(Ni, mod),
+//     product = bi*Ni*xi;
+
+//   return {
+//     bi,
+//     mod,
+//     Ni,
+//     xi,
+//     product
+//   };
+// }
+
+// const buses = [];
+// routes.forEach((route, idx) => {
+//   buses.push(createBus(route, offsets[idx], prod));
+// })
+
+// let sum = 0;
+// buses.forEach((bus) => sum += bus.product);
+
+// let reducedRemainder = sum - prod * Math.floor(sum/prod);
+
+// console.log(prod-reducedRemainder);
+// console.log(prod%reducedRemainder);
+// console.log(sum%prod);
+
+// Part two (someone's converted Python script)
+// create pairs of (divisor, remainder) for every available bus
+const buses = [];
 input[1].split(',').forEach((bus, idx) => {
   if (bus !== 'x') {
-    routes.push(+bus);
-    offsets.push(idx);
+    buses.push({
+      id: +bus,
+      mod: idx === 0 ? 0 : +bus - idx % +bus
+    });
   }
 });
 
-let prod = 1;
-routes.forEach((route) => prod *= route);
+console.log(buses);
 
-function getXi(Ni, mod) {
-  let a = Ni - (mod * Math.floor(Ni/mod));
+let result = 0,
+  increment = 1;
 
-  if (a === 1) {
-    return a;
+buses.forEach((bus) => {
+  while (result % bus.id !== bus.mod) {
+    result += increment;
   }
+  increment *= bus.id;
+});
 
-  let i = 2;
-  while ((i*a) % mod != 1) {
-    i += 1;
-  }
-
-  return i;
-}
-
-function createBus(id, offset, N) {
-  const bi = offset,
-    mod = id,
-    Ni = N / mod,
-    xi = getXi(Ni, mod),
-    product = bi*Ni*xi;
-
-  return {
-    bi,
-    mod,
-    Ni,
-    xi,
-    product
-  };
-}
-
-const buses = [];
-routes.forEach((route, idx) => {
-  buses.push(createBus(route, offsets[idx], prod));
-})
-
-let sum = 0;
-buses.forEach((bus) => sum += bus.product);
-
-let reducedRemainder = sum - prod * Math.floor(sum/prod);
-
-console.log(prod-reducedRemainder);
-console.log(prod%reducedRemainder);
+console.log(result);
